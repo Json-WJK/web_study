2ce4789ddee86b318de649289d564a7fb58291fd
王俊坤 <congti422@163.com>
2021-06-23 16:29:41 +0800

    编写对比代码

--- src/study/TaskSlice/TaskSlice.vue
+++ src/study/TaskSlice/TaskSlice.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class>
-    <div v-for="(item, index) in arr" :key="index">{{ item }}</div>
+  <div class="TaskSlice">
+    <!-- 正常渲染 -->
+    <div>
+      <h1>正常渲染</h1>
+      <div v-for="(item, index) in arr1" :key="index">{{ item }}</div>
+    </div>
+    <!-- 任务切片处理后渲染 -->
+    <div>
+      <h1>任务切片处理后渲染</h1>
+      <div v-for="(item, index) in arr2" :key="index">{{ item }}</div>
+    </div>
   </div>
 </template>
 <script>
@@ -8,16 +17,25 @@ import { reactive } from "vue";
 import TaskSlice from "./index.js";
 export default {
   setup() {
-    const arr = reactive([]);
+    const arr1 = reactive([]);
+    for (let i = 0; i < 20; i++) {
+      arr1.push(i);
+    }
+    const arr2 = reactive([]);
     TaskSlice.init({
       sliceList: 20,
       callback: function (i) {
-        arr.push(i)
+        arr2.push(i);
       },
     });
-    return { arr };
+    return { arr1, arr2 };
   },
 };
 </script>
 <style lang='less' scoped>
+.TaskSlice {
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+}
 </style>
